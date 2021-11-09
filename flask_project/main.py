423446d3eb4a2a41ec3a0d7b1eb55baf37bbdb5e
Autumn Horrell <ahorrell@uncc.edugit config -- global core.autocrlf inputgit config --global core.safecrlf warncd Downloads/git_tutorial/cdDownloads/git_tutorial.zip/git_tutorial/git config --global user.email ahorrell@uncc.edu>
2021-11-09 15:11:10 -0500

    flask project - added route to registration

--- flask_project/main.py
+++ flask_project/main.py
@@ -2,11 +2,18 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask
 from flask import render_template# Flask is the web app that we will cust
+from flask import redirect, url_for
 
 app = Flask(__name__)  #create an app
 
+#login
+@app.route('/')
 @app.route('/login')
 def login():
     return render_template('login.html')
+#registration
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)),debug=True)
