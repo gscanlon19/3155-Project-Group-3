dd1f03b84be712c6c7a0c29b811b6b2243b4bceb
Autumn Horrell <ahorrell@uncc.edugit config -- global core.autocrlf inputgit config --global core.safecrlf warncd Downloads/git_tutorial/cdDownloads/git_tutorial.zip/git_tutorial/git config --global user.email ahorrell@uncc.edu>
2021-11-09 20:23:16 -0500

    Flask project - added models

---
+++ flask_project/models.py
@@ -0,0 +1,8 @@
+From database import db
+
+class Post(db.Model):
+	id = db.Column("id", db.Integer, primary_key = True)
+	text = db.Column("text", db.String(100))
+
+	def __init__(self, text):
+		self.text
