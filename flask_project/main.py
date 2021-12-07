8ea8e0322ccdc46c0833cc4d93c40e09d8e54f05
Autumn Horrell <ahorrell@uncc.edugit config -- global core.autocrlf inputgit config --global core.safecrlf warncd Downloads/git_tutorial/cdDownloads/git_tutorial.zip/git_tutorial/git config --global user.email ahorrell@uncc.edu>
2021-12-07 09:42:57 -0500

    compress file

--- flask_project/main.py
+++ flask_project/main.py
@@ -1,8 +1,8 @@
 # imports
-import os  # os is used to get environment variables IP & PORT
+import os  
 import bcrypt
 from flask import Flask
-from flask import render_template  # Flask is the web app that we will cust
+from flask import render_template
 from flask import redirect, url_for
 from flask import request
 from database import db
@@ -17,11 +17,11 @@ app = Flask(__name__)  # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_project_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SE3155'
-#  Bind SQLAlchemy db object to this Flask app
+
 db.init_app(app)
-# Setup models
+
 with app.app_context():
-    db.create_all()  # run under the app context
+    db.create_all()
 
 
 @app.route('/')
@@ -67,24 +67,22 @@ def filters():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     login_form = LoginForm()
-    # validate_on_submit only validates using POST
+
     if login_form.validate_on_submit():
-        # we know user exists. We can use one()
+
         the_user = db.session.query(User).filter_by(email=request.form['email']).one()
-        # user exists check password entered matches stored password
+
         if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
-            # password match add user info to session
+
             session['user'] = the_user.first_name
             session['user_id'] = the_user.id
-            # render view
+
             return redirect(url_for('get_posts'))
 
-        # password check failed
-        # set error message to alert user
         login_form.password.errors = ["Incorrect username or password."]
         return render_template("login.html", form=login_form)
     else:
-        # form did not validate or GET request
+
         return render_template("login.html", form=login_form)
 
 
@@ -185,30 +183,29 @@ def register():
     form = RegisterForm()
 
     if request.method == 'POST' and form.validate_on_submit():
-        # salt and hash password
+
         h_password = bcrypt.hashpw(
             request.form['password'].encode('utf-8'), bcrypt.gensalt())
-        # get entered user data
+
         first_name = request.form['firstname']
         last_name = request.form['lastname']
-        # create user model
+
         new_user = User(first_name, last_name, request.form['email'], h_password)
-        # add user to database and commit
+
         db.session.add(new_user)
         db.session.commit()
-        # save the user's name to the session
+
         session['user'] = first_name
-        session['user_id'] = new_user.id  # access id value from user model of this newly added user
-        # show user dashboard view
+        session['user_id'] = new_user.id
+
         return redirect(url_for('get_posts'))
 
-    # something went wrong - display register view
     return render_template('register.html', form=form)
 
 
 @app.route('/logout')
 def logout():
-    # check if a user is saved in session
+
     if session.get('user'):
         session.clear()
 
@@ -219,9 +216,9 @@ def logout():
 def new_comment(post_id):
     if session.get('user'):
         comment_form = CommentForm()
-        # validate_on_submit only validates using POST
+
         if comment_form.validate_on_submit():
-            # get comment data
+
             comment_text = request.form['comment']
             new_record = Comment(comment_text, int(post_id), session['user_id'])
             db.session.add(new_record)
